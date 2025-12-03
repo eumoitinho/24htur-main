5a44ad476e9db0ceaf4f1b3d11bd929c47a39707
João Vitor <63624487+eumoitinho@users.noreply.github.com>
2025-12-03 13:31:32 +0000

    feat: Adicionar arquivo index.js para exportação de componentes corporativos

---
+++ components/corporate/index.js
@@ -0,0 +1,8 @@
+export { default as HeaderCorporate } from './HeaderCorporate';
+export { default as HeroCorporate } from './HeroCorporate';
+export { default as ClientsCorporate } from './ClientsCorporate';
+export { default as DilemaCorporate } from './DilemaCorporate';
+export { default as SolucaoCorporate } from './SolucaoCorporate';
+export { default as TestimonialsCorporate } from './TestimonialsCorporate';
+export { default as OfertaCorporate } from './OfertaCorporate';
+export { default as FooterCorporate } from './FooterCorporate';
